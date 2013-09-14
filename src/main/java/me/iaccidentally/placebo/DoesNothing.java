81568ddaece8b41253c8cf8e412ff0520acae68a
Iaccidentally <coryhuckaby@gmail.com>
2013-09-14 15:22:25 -0400

    OH-EM-GEE FORMATTING

--- src/main/java/me/iaccidentally/placebo/DoesNothing.java
+++ src/main/java/me/iaccidentally/placebo/DoesNothing.java
@@ -1,8 +1,10 @@
 package me.iaccidentally.placebo;
 
-public class DoesNothing {
 
-	public void doNothing() {
+public class DoesNothing
+{
+	public void doNothing()
+	{
 		//Let's do something special here! Oh wait... Plugin does nothing, so we keep doing nothing.
 	}
 }
