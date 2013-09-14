18d459ef23fd4659442a558918dbe17bdc349d58
recon88 <info@ingoguther.de>
2013-09-14 21:14:22 +0200

    Added DoesNothing to do nothing

---
+++ src/main/java/me/iaccidentally/placebo/DoesNothing.java
@@ -0,0 +1,8 @@
+package me.iaccidentally.placebo;
+
+public class DoesNothing {
+
+	public void doNothing() {
+		//Let's do something special here! Oh wait... Plugin does nothing, so we keep doing nothing.
+	}
+}
